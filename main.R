2fb59ce7827075fb30a70ac38210e73dd32eea08
marcteys <marc.teys@gmail.com>
2020-03-03 18:55:29 +0100

    export fonctionnel

--- main.R
+++ main.R
@@ -1,4 +1,15 @@
 
+#  Dependances 
+library(tigerstats)
+
+library(stringr)
+library(ggplot2)
+library(gridExtra)
+install.packages("egg") 
+library("egg")
+install.packages("devtools")
+library(devtools)
+
 
 # Charger le document principal
 
@@ -22,90 +33,98 @@ header
 
 #### Stats 
 
-rowPerc(xtabs(~ Genre + PressionSexe, data))
-
-str(factor(header[1,"PressionSexe"]), max.levels=0)
-
-
-
-
-library(ggplot2)
-tmp = data.frame()
-tmp = xtabs(~ Genre + PressionSexe, dataFemmesHommes)
-tmp = as.data.frame(tmp) 
-
-tmpPercent = data.frame()
-
-tmpPercent = rowPerc(xtabs(~ Genre + PressionSexe, dataFemmesHommes))
-tmpPercent = as.data.frame(tmpPercent) 
-
-tmpPercent = head(tmpPercent,-2)
-
-
-tmp = arrange(tmp, -row_number()) # reverse row orders to display yes and now first
-tmpPercent = arrange(tmpPercent, -row_number()) # reverse row orders to display yes and now first
-tmpPercent
-tmp$PressionSexe <- factor(tmp$PressionSexe, levels = rev(levels(tmp$PressionSexe)))
-tmpPercent$PressionSexe <- factor(tmpPercent$PressionSexe, levels = rev(levels(tmpPercent$PressionSexe))) # Reverse factors
-#df_transpose = t(tmp)
-
-
-
-
-str(factor(header[1,"PressionSexe"]), max.levels=0)
-
-
-
-mycols <- c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF")
-
-plottitle = c(as.character(header[1,"PressionSexe"]))
-
-plot <- ggplot(data = tmpPercent, aes(x = 2, y = Freq, fill = PressionSexe)) + 
-  geom_bar(width = 1, stat = "identity", color ="white", position = position_fill()) +
-  geom_text(aes(label = Freq), position = position_fill(vjust = 0.5), color ="white") +
-  coord_polar(theta = "y") +
-  facet_wrap(~Genre )  +
-  scale_fill_manual(values = mycols) +
-  theme_void() +  
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank()) + 
-  theme(legend.position='right') + 
- xlim(0.5, 2.5) +
-  labs(fill  = "", title = plottitle) +
-  theme(plot.title = element_text(hjust=0.5)) + 
-  theme(legend.key.height=unit(1, "cm")) 
- #+  theme(plot.title = element_text(size=1000/nchar(plottitle), vjust=10))
-plot 
 
 
 
 
 
 
-GetInfoParGenre(data, "PressionSexe")
 
 
 
-GetInfoParGenre(data, "SexePenetrationEveille")
+
+## FONCTIONS A CHARGER
+
+wrap_strings <- function(vector_of_strings,width){sapply(vector_of_strings,FUN=function(x){paste(strwrap(x,width=width), collapse="\n")})} 
 
 
-library(tigerstats)
-
-
-
-GetInfoParGenre <- function(data,column = "") {
+AfficherGraphGenre <- function(data,columnName = "", type = "percent", reverse = TRUE, removeEmpty = FALSE) {
  
+  # Clear
+  if(removeEmpty)
+    data = data[-which(data[,c(columnName)] == ""), ]
   
-  result <- rowPerc(xtabs(~ Genre + eval(parse(text = column)), data))
+  tmp = data.frame()
+   # Regarder si c'est vide 
+  tmp = xtabs(~ Genre + data[, c(columnName)], data)
+  tmp = as.data.frame(tmp) 
+  names(tmp)[2] <- columnName# Fix erreur de la première column
+  # Remove cleared rows
+  if(removeEmpty)
+  tmp = tmp[-which(tmp$Freq == "0"), ] #clear empty things
   
-  print(factor(header[1,column]), max.levels=0)
+  tmpPercent = data.frame()
   
-  result
+  tmpPercent = rowPerc(xtabs(~ Genre + data[, c(columnName)], data)  )
+  tmpPercent = as.data.frame(tmpPercent) 
+  names(tmpPercent)[2] <- columnName# Fix erreur de la première column
+  print(tmpPercent)
+  if(removeEmpty)
+  tmpPercent = tmpPercent[-which(tmpPercent$Freq == "0"), ] #clear empty things
   
-  return(result)
+  tmpPercent = head(tmpPercent,-2) # on enleve les total
+  tmp = head(tmp,-2)
+  
+  if(reverse) {
+  
+  tmp = arrange(tmp, -row_number()) # reverse row orders to display yes and now first
+  tmpPercent = arrange(tmpPercent, -row_number()) # reverse row orders to display yes and now first
+  
+  
+  tmp[, c(columnName)] <- factor(tmp[, c(columnName)], levels = rev(levels(tmp[, c(columnName)])) )
+  tmpPercent[, c(columnName)] <- factor(tmpPercent[, c(columnName)], levels = rev(levels(tmpPercent[, c(columnName)] ))) # Reverse factors
+  #df_transpose = t(tmp)
+  }
+  
+  #tmpPercent = head(tmpPercent,-4) # On eneleve les je ne sais pas 
+  #tmp = head(tmp,-4)
+  
+  
+  mycols <- c("#a132b8FF","#d9aee2ff", "#f3e4f6FF","#f4a5c1ff" , "#f3c2b4ff", "#f0f1f2ff","#a132b8FF","#d9aee2ff", "#f3e4f6FF","#f4a5c1ff" , "#f3c2b4ff", "#f0f1f2ff")
+  textCol <- c("black", "black", "white","black","black", "black","black", "black", "white","black","black", "black")
+  plottitle = factor(header[1,columnName])
+  plottitle = str_replace(plottitle, "\n", "\n")
+  plottitle
+  
+  
+  plot <- ggplot(data = tmpPercent, aes(x = 2, y = Freq, fill = tmpPercent[,c(columnName)] )) + 
+    geom_bar(width = 1, stat = "identity", color ="white", position = position_fill()) +
+    geom_text(aes(label = round(Freq)), position = position_fill(vjust = 0.5), color ="black",size=4) +
+    coord_polar(theta = "y") +
+    facet_wrap(~Genre,strip.position="bottom")  +
+    scale_fill_manual(values = mycols) +
+    theme_void() +  
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank()) + 
+    theme(legend.position='right') + 
+    xlim(0.5, 2.5) +
+    labs(fill  = "", title = wrap_strings(plottitle, 50)) +
+    # theme(plot.title = element_text(hjust=0.5)) + 
+    theme(legend.key.height=unit(1, "cm")) +
+    theme(legend.key.size = unit(0.5, "cm")) +
+    theme(plot.margin=unit(c(1,1,1.5,1.2),"cm")) +
+    theme(strip.text = element_text(size=15) ) +  
+    theme( legend.margin = margin(5, 50, 5, 50) ) +
+    #theme(plot.title = element_text(size=30/nchar(plottitle), vjust=-200))+
+    theme(plot.title = element_text(size=13, hjust = 0.5))
+  
+  
+  return(plot)
 }
 
+p1 = AfficherGraphGenre(dataFemmesHommes, "SexeHorsPenetrationSommeil", reverse = TRUE)
+p2 = AfficherGraphGenre(dataFemmesHommes, "SexeHorsPenetrationSommeilPartenaire", reverse = FALSE, removeEmpty = TRUE)
 
-
-
+ggarrange(p1, p2, ncol = 1) %>%
+ggexport(filename = "test.png") 
 
